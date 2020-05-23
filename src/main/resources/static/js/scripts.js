cd1fed2df10410046f4d205c1f4ebf23d3f4556d
Nathaly Quintero Aristizabal <nqaristizabal@gmail.com>
2020-05-23 15:32:04 -0500

    Avance vista categoria pijamas mujer

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -24,3 +24,24 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
   
     return false;
   });
+
+  /*Extiende el tamaño de un elemento para mostrar más información*/
+  function masInfoPijama(x){
+    x.children[1].children[2].style.display = 'block';
+    x.children[1].children[3].style.display = 'block';
+  }
+
+  /*Deja de forma original el tamaño y esconde información adicional de un elemento*/
+  function menosInfoPijama(x){
+    x.style.height = "auto";
+    x.children[1].children[2].style.display = 'none';
+    x.children[1].children[3].style.display = 'none';
+  }
+
+  function showElement(x){
+    x.style.display = "block";
+  }
+
+  function hideElement(x){
+    x.style.display = "none";
+  }
